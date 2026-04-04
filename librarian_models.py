677b0a82f95174066e279246e6ac41cf1007c1e3
ADRISH KUMAR BANERJEE <adrish@Mac.lan>
2026-04-04 19:14:36 +0530

    Describe what you changed

--- librarian_models.py
+++ librarian_models.py
@@ -1,18 +1,49 @@
 import json
 
 #viewing a student details
-def view_student(students_list,books_list,std_index,book_index):
-    print(f"Name: {students_list[std_index]["name"]}")
-    print(f"Roll Number: {students_list[std_index]["roll"]}")
-    print(f"Department: {students_list[std_index]["department"]}")
-    print(f"Year: {students_list[std_index]["year"]}")
+def view_student(students_list, books_list, std_index):
+    print(f"Name: {students_list[std_index]['name']}")
+    print(f"Roll Number: {students_list[std_index]['roll']}")
+    print(f"Department: {students_list[std_index]['department']}")
+    print(f"Year: {students_list[std_index]['year']}")
     print("Issued Books:")
-    for x in students_list[std_index]["books_issued"]:
+    for i in range(len(students_list[std_index]["books_issued"])):
+        book_code = students_list[std_index]["books_issued"][i]
         for y in books_list:
-            print(books_list[y]["code"])
+            if y["code"] == book_code:
+                print(f"{y['name']} (Code: {book_code})")
+                print(f"Issue Date: {students_list[std_index]['issue_date'][i]}")
+                print(f"Scheduled Return: {students_list[std_index]['scheduled_return_date'][i]}")
+                if i < len(students_list[std_index]["return_date"]):
+                    print(f"Actual Return: {students_list[std_index]['return_date'][i]}")
+                else:
+                    print("Actual Return: Not Returned")
+                print("-" * 30)
+                break
+    print("Requested Books:")
+    for i in students_list[std_index]["books_requested"]:
+        book_code=i
+        for y in range(len(books_list)):
+            if books_list[y]["code"] == book_code:
+                print(f"{books_list[y]["name"]}\n")
+    print("Late Returned Books:")
+    for i in students_list[std_index]["late_returned_books"]:
+        book_code=i
+        for y in range(len(books_list)):
+            if books_list[y]["code"] == book_code:
+                print(f"{books_list[y]["name"]}\n")
+    print("Damaged Books: ")
+    for i in students_list[std_index]["damaged_books"]:
+        book_code=i
+        for y in range(len(books_list)):
+            if books_list[y]["code"] == book_code:
+                print(f"{books_list[y]["name"]}\n")
+    print(f"Total Fine: {students_list[std_index]['fine']}")
+    print(f"Fine Cleared: {students_list[std_index]['fine_cleared']}")
+    print(f"Books with student: {students_list[std_index]['books_with_user']}")
 
 
-
+# test change
 #adding a new student
 def add_student(students_list):
     name=input("Enter Student Name: ")
